c4a7756c6ca849b10669f2ad123a2d70be1a52e8
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-10-28 08:32:08 +1100

    add code formatting fixes for vscode

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,16 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import { Tab } from '../node_modules/bootstrap';
+  import { Tab } from '../node_modules/bootstrap'
   import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
-
-  
   <div class="container py-4 px-3 mx-auto">
     <h1>DJPR labour dashboard</h1>
-    <p>The DJPR labour dashboard explores information on Victorian employment and job seekers. Use the sidebar to navigate through information on various demographics such as regions, age, sex and industry.</p>
-    <Counter count = {10}/>
+    <p>
+      The DJPR labour dashboard explores information on Victorian employment and
+      job seekers. Use the sidebar to navigate through information on various
+      demographics such as regions, age, sex and industry.
+    </p>
+    <Counter count={10} />
   </div>
-
 </main>
-
