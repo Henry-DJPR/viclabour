0f5c6b039de561e29d3cd8849ddfe237cdf21b59
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-10-27 16:47:45 +1100

    sass setup

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,10 @@
 <script>
-  let count = 0
+  export let count = 0;
   const increment = () => {
     count += 1
   }
 </script>
 
-<button on:click={increment}>
+<button class = "btn btn-primary" on:click={increment}>
   count is {count}
 </button>
