4360b9d5aef59f17e9e443900a3f152669f564ff
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-10-28 08:05:27 +1100

    add sass

--- src/App.svelte
+++ src/App.svelte
@@ -5,28 +5,12 @@
 </script>
 
 <main>
-  <div>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-
+  
   <div class="container py-4 px-3 mx-auto">
-    <h1>Hello, Bootstrap and Vite!</h1>
-    <button class="btn btn-primary">Primary button</button>
+    <h1>DJPR labour dashboard</h1>
+    <p>The DJPR labour dashboard explores information on Victorian employment and job seekers. Use the sidebar to navigate through information on various demographics such as regions, age, sex and industry.</p>
+    <Counter count = {10}/>
   </div>
 
 </main>
