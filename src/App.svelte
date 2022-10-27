69b9ddf560e06bfe9cb83ced392d95e40f99f8d8
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-10-27 16:19:58 +1100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import { Tab } from '../node_modules/bootstrap';
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div>
+    <a href="https://svelte.dev" target="_blank"> 
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+
+  <div class="container py-4 px-3 mx-auto">
+    <h1>Hello, Bootstrap and Vite!</h1>
+    <button class="btn btn-primary">Primary button</button>
+  </div>
+
+</main>
+
