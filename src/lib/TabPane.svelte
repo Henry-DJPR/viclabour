8fcb0ef0477fe84c1791c8faa156f27e687e90b5
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-11-16 14:21:42 +1100

    new lazy load (highcharts nead to be visible)

--- src/lib/TabPane.svelte
+++ src/lib/TabPane.svelte
@@ -3,18 +3,36 @@
   export let name = 'Test'
   export let active = false
   let paneClass = ''
+  let paneWidth
+  let visibleCheck = false
 
   if (active === true) {
     paneClass = 'tab-pane active fade show'
   } else {
     paneClass = 'tab-pane fade'
   }
+
+  function updateVisibleCheck(w) {
+    if (w > 0) {
+      visibleCheck = true
+    }
+  }
+
+  $: updateVisibleCheck(paneWidth)
 </script>
 
-<div class={paneClass} {id} role="tabpanel" aria-labelledby={'tab-' + id}>
-  <slot>
-    <h2>
-      {name} tabpanel
-    </h2>
-  </slot>
+<div
+  class={paneClass}
+  {id}
+  role="tabpanel"
+  aria-labelledby={'tab-' + id}
+  bind:clientWidth={paneWidth}
+>
+  {#if visibleCheck === true}
+    <slot>
+      <h2>
+        {name} tabpanel
+      </h2>
+    </slot>
+  {/if}
 </div>
